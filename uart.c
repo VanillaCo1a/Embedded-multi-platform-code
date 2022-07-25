c3ac2c23e291d98005008c77ed27ed9ea8373424
Vn1aC0la <Peter.kp.Xu@gmail.com>
2022-07-25 20:41:04 +0800

    change.B_uart:添加一个128B的缓冲区，用于所有的可变参format输出
        添加const修饰符，告诉调用者哪些参数将不会被修改

--- uart.c
+++ uart.c
@@ -1,19 +1,23 @@
 ﻿#include "uart.h"
 #include "usart.h"
 
-UART_ModuleHandleTypeDef muart1 = {.receive = {.state = 0}, .transmit = {.state = 1}, .usedma = 0};
-DEVCMNI_TypeDef uart1_cmni = {.protocol = USART, .ware = HARDWARE, .modular = &muart1,
+static UART_ModuleHandleTypeDef msuart[UART_NUM] = {
+    {.receive = {.state = 0}, .transmit = {.state = 1}, .usedma = 0},
+};
+static DEVCMNI_TypeDef uart_cmni[UART_NUM] = {
+    {.protocol = USART, .ware = HARDWARE, .modular = &msuart[0],
 #if defined(STM32)
 #if defined(STM32HAL)
-                              .bus = &huart1
+     .bus = &huart1
 #elif defined(STM32FWLIB)
-                              .bus = USART1
+     .bus = USART1
 #endif
 #endif
+    },
 };
 DEVS_TypeDef uarts = {.type = UART};
 DEV_TypeDef uart[UART_NUM] = {
-    {.parameter = NULL, .io = {0}, .cmni = {.num = 1, .confi = (DEVCMNI_TypeDef *)&uart1_cmni, .init = NULL}}};
+    {.parameter = NULL, .io = {0}, .cmni = {.num = 1, .confi = (DEVCMNI_TypeDef *)&uart_cmni[0], .init = NULL}}};
 
 /* 串口初始化函数 */
 void UART_Init(void) {
@@ -27,27 +31,52 @@ void UART_Init(void) {
     printf("UART1初始化完毕\r\n");
 }
 
-bool UART1_ScanArray(uint8_t arr[], size_t size, size_t *length) {
-    DEV_setActStream(&uarts, 0);
-    return DEVCMNI_Read(arr, size, length, 0xFF);
+
+/* TOFIX: 由于设备序号和设备号的对应关系处理还不是很完善, 这个函数还不能作为对外的调用接口 */
+static inline bool UART_ScanArray(int8_t num, uint8_t arr[], size_t size, size_t *length) {
+    DEV_setActStream(&uarts, num);
+    return DEVCMNI_Read((uint8_t *)arr, size, length, 0xFF);
 }
-bool UART1_ScanString(char *str, size_t size) {
+static inline bool UART_ScanString(int8_t num, char *str, size_t size) {
     bool res = false;
     size_t length;
-    DEV_setActStream(&uarts, 0);
+    DEV_setActStream(&uarts, num);
     res = DEVCMNI_Read((uint8_t *)str, size - 1, &length, 0xFF);
     if(res) {
         str[length] = '\0';
     }
     return res;
 }
-void UART1_PrintArray(uint8_t arr[], size_t size) {
-    DEV_setActStream(&uarts, 0);
-    while(!DEVCMNI_Write(arr, size, 0xFF)) continue;
+static inline void UART_PrintArray(int8_t num, const uint8_t arr[], size_t size) {
+    DEV_setActStream(&uarts, num);
+    while(!DEVCMNI_Write((uint8_t *)arr, size, 0xFF)) continue;
 }
-void UART1_PrintString(char *str) {
-    DEV_setActStream(&uarts, 0);
+static inline void UART_PrintString(int8_t num, const char *str) {
+    DEV_setActStream(&uarts, num);
     while(!DEVCMNI_Write((uint8_t *)str, strlen(str), 0xFF)) continue;
+}
+
+#define VA_BUF_SIZE 1024
+static char va_buf[VA_BUF_SIZE] = {0};
+bool UART1_ScanArray(uint8_t arr[], size_t size, size_t *length) {
+    return UART_ScanArray(0, arr, size, length);
+}
+bool UART1_ScanString(char *str, size_t size) {
+    return UART_ScanString(0, str, size);
+}
+void UART1_PrintArray(const uint8_t arr[], size_t size) {
+    UART_PrintArray(0, arr, size);
+}
+void UART1_PrintString(const char *str) {
+    UART_PrintString(0, str);
+}
+void UART1_Printf(char *str, ...) {
+    va_list args;
+    va_start(args, str);
+    vsnprintf(va_buf, sizeof(va_buf), (char *)str, args);
+    va_end(args);
+
+    UART_PrintString(0, va_buf);
 }
 
 /* printf重定向 */
--- uart.h
+++ uart.h
@@ -1,12 +1,14 @@
 #ifndef __UART_H
 #define __UART_H
 #include "device.h"
-#define UART_NUM 1
 
+#define UART_NUM 2
 void UART_Init(void);
-bool UART1_ScanArray(uint8_t arr[], size_t size, size_t *length);
+
+bool UART1_ScanArray(uint8_t *arr, size_t size, size_t *length);
 bool UART1_ScanString(char *str, size_t size);
-void UART1_PrintArray(uint8_t arr[], size_t size);
-void UART1_PrintString(char *str);
+void UART1_PrintArray(const uint8_t *arr, size_t size);
+void UART1_PrintString(const char *str);
+void UART1_Printf(char *str, ...);
 
 #endif
